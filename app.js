53c38bef6a0ae9982ac2947d1e5a0b24cd22827b
Cade Nichols <cade@cadenichols.com>
2016-04-23 15:02:58 -0700

    new todo modal

--- app.js
+++ app.js
@@ -6,18 +6,28 @@ var express = require('express');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 
+var Todo = require('./models/todo');
 
 var app = express();
 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-
 app.use(express.static('public'));
-
+app.set('view engine', 'ejs')
 
 app.use('/api', require('./routes/api'));
 
+app.get('/', (req, res) => {
+
+  Todo.get((err, todos) => {
+    if(err) {
+      res.render('error', {error: err})
+    } else {
+      res.render('home', {todos: todos});
+    }
+  })
+})
 
 app.listen(PORT, err => {
   console.log(err || `Server listening on port ${PORT}`);
